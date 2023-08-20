e71966b8c58504dec60f25b5af1ab50ee8768595
Akbar <muhammad.akbar@arkamaya.co.id>
2023-08-20 21:24:52 +0700

    push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,8 @@
 <template>
   <main>
     <!-- <TheWelcome /> -->
-    <div class="container">
-      <div class="row">
+    <div class="container" style="min-height: fit-content">
+      <div class="row" style="margin-top: 100px">
         <div class="col-md-12">
           <section class="jumbotron text-center">
             <img
@@ -15,19 +15,16 @@
             />
             <h1 class="display-6 fw-bold">Muhammad Akbar</h1>
             <p>Backend Developer</p>
-            <a href="http://blog.muhakbar.com/" class="btn btn-primary"
-              ><i class="bi bi-box-arrow-up-right"></i> Blog</a
-            >
             <a
               href="https://drive.google.com/file/d/1ZNHLrb_EfqVncFDvGGVlc-ot9DaaVSx1/view?usp=sharing"
               target="_blank"
-              class="btn btn-primary"
+              class="btn btn-primary m-2"
               ><i class="bi bi-cloud-arrow-down-fill"></i> CV Akbar</a
             >
             <a
               href="https://drive.google.com/file/d/1Fct6hbaPOsVlSHhn71RtD34MW8O2uD-C/view?usp=sharing"
               target="_blank"
-              class="btn btn-primary"
+              class="btn btn-primary m-2"
               ><i class="bi bi-cloud-arrow-down-fill"></i> Portofolio</a
             >
           </section>
